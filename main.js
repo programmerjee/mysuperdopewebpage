088c93be57c1e75b49937ef7abbb3578c2441668
programmerjee <angelarowe7@gmail.com>
2017-11-27 18:58:06 -0500

    fixed description

--- main.js
+++ main.js
@@ -8,27 +8,18 @@ jQuery.fn.loadRepositories = function(username) {
     var target = this;
     $.githubUser(username, function(data) {
         var repos = data.data;
-        sortByDate(repos);    
+        sortByName(repos);    
    
         var list = $('<dl/>');
         target.empty().append(list);
         $(repos).each(function() {
             if (this.name != (username.toLowerCase()+'.github.com')) {
                 list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
-                list.append('<dd>' + '<img src ="' + this.description + '" width="100"></img>' +'</dd>');
+                list.append('<dd>' +  (this.description) +'</dd>');
                 list.append('<dd>' + this.updated_at + '</dd>');
             }
         });      
      });
-    function sortByDate(repos) {
-        repos.sort(function(a,b) {
-            if(a.updated_at < b.updated_at) {
-                return -1;
-            }
-        } return a.updated_at - b.updated_at;
-            });
-        }
-    };
       
    function sortByName(repos) {
         repos.sort(function(a,b) {
