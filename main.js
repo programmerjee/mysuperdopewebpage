73acd24b4e5e8318a73638d52c5ee63ac8f7f2a1
programmerjee <angelarowe7@gmail.com>
2017-11-27 18:11:34 -0500

    updated jquery

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-jQuery.githubUser = function(username, callback) {
+jjQuery.githubUser = function(username, callback) {
    jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback)
 }
- 
+
 jQuery.fn.loadRepositories = function(username) {
     this.html("<span>Querying GitHub for " + username +"'s repositories...</span>");
-     
+    
     var target = this;
     $.githubUser(username, function(data) {
         var repos = data.data; // JSON Parsing
         sortByName(repos);    
-     
+   
         var list = $('<dl/>');
         target.empty().append(list);
         $(repos).each(function() {
             if (this.name != (username.toLowerCase()+'.github.com')) {
                 list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
-                list.append('<dd>' + this.description +'</dd>');
-              list.append('<dd>' + this.updated_at +'</dd>');
+                list.append('<dd>' + '<img src ="' + this.description + '" width="100"></img>' +'</dd>');
+                list.append('<dd>' + this.updated_at + '</dd>');
             }
         });      
-      });
+     });
       
-    function sortByName(repos) {
+   function sortByName(repos) {
         repos.sort(function(a,b) {
         return a.name - b.name;
        });
